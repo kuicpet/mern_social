c7065af5fdd624ca09b68290df240cf4692c8c69
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2022-08-15 14:42:22 +0100

    ft--updated client

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,11 +9,17 @@ import {
   compose,
 } from 'redux'
 import thunk from 'redux-thunk'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import reducers from './reducers'
 
+
 const store = createStore(reducers, compose(applyMiddleware(thunk)))
+const clientId = import.meta.env.VITE_GOOGLE_TOKEN
+const clientSecret = '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'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <GoogleOAuthProvider clientId={clientId}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </GoogleOAuthProvider>
 )
