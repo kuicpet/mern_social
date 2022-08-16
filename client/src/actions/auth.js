ea4a5e9ee2b85f38d4117abbc8f2723142ad7218
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2022-08-16 13:29:00 +0100

    ft--updated authentication

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,21 +1,24 @@
-import { AUTH } from "../constanta/actionTypes";
+import { AUTH } from '../constanta/actionTypes'
 import * as api from '../api'
 
-export const signin = (formData, navigate) => async(dispatch) => {
-    try {
-        // login the user
-        navigate('/')
-    } catch (error) {
-        console.log(error)
-    }
+export const signin = (formData, navigate) => async (dispatch) => {
+  try {
+    // login the user
+    const { data } = await api.signIn(formData)
+    dispatch({ type: AUTH, data })
+    navigate('/')
+  } catch (error) {
+    console.log(error)
+  }
 }
 
-
-export const signup = (formData, navigate) => async(dispatch) => {
-    try {
-        // signup the user
-        navigate('/')
-    } catch (error) {
-        console.log(error)
-    }
+export const signup = (formData, navigate) => async (dispatch) => {
+  try {
+    // signup the user
+    const { data } = await api.signUp(formData)
+    dispatch({ type: AUTH, data })
+    navigate('/')
+  } catch (error) {
+    console.log(error)
+  }
 }
--- client/src/components/Navbar/styles.js
+++ client/src/components/Navbar/styles.js
@@ -50,6 +50,7 @@ export default makeStyles((theme) => ({
     display: 'flex',
     alignItems: 'center',
     textAlign: 'center',
+    color: '#000'
   },
   brandContainer: {
     display: 'flex',
